5e28e249f4492844d3b1f93020ac62bfad54d9ca
MikulkovaZ <mikulkova.zuzana@seznam.cz>
2021-04-17 10:33:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,14 @@
 // První příklad - vypiš vzestupně čísla od 0 do 10
 console.log('První příklad - vzestupná řada 0 - 10');
 
-//TADY NAPIS CYKLUS
+let a=0;
+
+while (a<=10) {
+    console.log(a);
+    a++;
+}
+
+
 
 console.log('-------------------');
 
@@ -16,8 +23,22 @@ console.log('-------------------');
 // v podmínce uvnitř cyklu
 
 console.log('Druhý příklad - rozdíl mezi <= a <');
+console.log('Druhý příklad - <')
 
-//TADY NAPIS CYKLUS
+let b=0;
+
+while (b<10) {
+    console.log(b);
+    b++;
+}
+
+console.log('Druhý příklaf - <=');
+let c=0;
+
+while (c<=10) {
+    console.log(c);
+    c++;
+}
 
 console.log('-------------------');
 
@@ -26,6 +47,11 @@ console.log('-------------------');
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
-//TADY NAPIS CYKLUS
+let d=10;
+
+while (d>=0) {
+    console.log(d);
+    d--;
+}
 
 console.log('-------------------');
